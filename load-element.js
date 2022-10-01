44b56e73fa5c883f800e015e2ee5209e7e60a104
Marek Zielina <github@niurop.com>
2022-10-01 22:27:42 +0200

    Make LoadElement into HTMLElement & Added definition for HTML
    Added observedAttributes with eval(...) for functions and calling load when first setting url.

--- load-element.js
+++ load-element.js
@@ -1,4 +1,4 @@
-class Load {
+class LoadElement extends HTMLElement {
   onstart = null;
   onend = null;
   onprogress = null;
@@ -42,4 +42,17 @@ class Load {
 
     this.onstart && this.onstart();
   }
+
+  static get observedAttributes() {
+    return ["url", "onstart", "onend", "onprogress", "onerror"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (["onstart", "onend", "onprogress", "onerror"].indexOf(name) > -1)
+      this[name] = eval(newValue);
+    else this[name] = newValue;
+    if (name == "url" && oldValue === null) this.load();
+  }
 }
+
+customElements.define("load-element", LoadElement);
